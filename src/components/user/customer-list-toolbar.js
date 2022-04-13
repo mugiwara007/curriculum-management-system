ebdea29f3b5ce288f2fed3e21cbd3785fcfc0f2f
jicenteno <centeno.johnisrael.j.1911@gmail.com>
2022-04-13 14:53:19 +0800

    user add modal

--- src/components/user/customer-list-toolbar.js
+++ src/components/user/customer-list-toolbar.js
@@ -14,6 +14,61 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import ArchiveIcon from '@mui/icons-material/Archive';
 
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import * as React from 'react';
+
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div style={{display : 'inline-block'}} >
+      <Button
+          color="primary"
+          variant="contained"
+          startIcon={(<AddIcon fontSize="small" />)}
+          onClick={handleClickOpen}
+        >
+          Add Users
+        </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+      </div>
+  );
+}
+
 export const CustomerListToolbar = (props) => (
   <Box {...props}>
     <Box
@@ -44,13 +99,8 @@ export const CustomerListToolbar = (props) => (
         >
           Archive
         </Button>
-        <Button
-          color="primary"
-          variant="contained"
-          startIcon={(<AddIcon fontSize="small" />)}
-        >
-          Add Users
-        </Button>
+        <FormDialog>
+        </FormDialog>
       </Box>
     </Box>
     <Box sx={{ mt: 3 }}>
