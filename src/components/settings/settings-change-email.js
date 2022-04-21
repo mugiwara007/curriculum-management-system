b2250da85a2e6bc8f5c5f88e7a133d1f6fcee524
AaronKarl <87815612+AaronKarl@users.noreply.github.com>
2022-04-20 19:32:02 -1200

    emaial

---
+++ src/components/settings/settings-change-email.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { Box, Button, Card, CardContent, CardHeader, Divider, TextField } from '@mui/material';
+
+export const SettingsEmail = (props) => {
+  const [values, setValues] = useState({
+    email: '',
+    newemail: ''
+  });
+
+  const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  return (
+    <form {...props}>
+      <Card>
+        <CardHeader
+          subheader="Use a password you have not used before"
+          title="Change email"
+        />
+        <Divider />
+        <CardContent>
+          <h3>Current</h3>
+          <TextField
+            fullWidth
+            label="Current Email"
+            margin="normal"
+            name="currentemail"
+            onChange={handleChange}
+            type="email"
+            value={values.current}
+            variant="outlined"
+          />
+          <h3>New</h3>
+          <TextField
+            fullWidth
+            label="New Email"
+            margin="normal"
+            name="newemail"
+            onChange={handleChange}
+            type="email"
+            value={values.new}
+            variant="outlined"
+          />
+        </CardContent>
+        <Divider />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            p: 1
+          }}
+        >
+          <Button
+            color="primary"
+            variant="contained"
+          >
+            Update Email
+          </Button>
+        </Box>
+      </Card>
+    </form>
+  );
+};
