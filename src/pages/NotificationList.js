1ccbf8de49790c1c8b5418fa94f41760bce3f647
Shrosch <lancelarit@yahoo.com>
2022-05-07 22:17:13 +0800

    Create Curriculum Responsiveness
    Erased Dummy data ng Notifications and History

--- src/pages/NotificationList.js
+++ src/pages/NotificationList.js
@@ -47,36 +47,11 @@ function NotificationDiv(props){
   }}
   >
     <ListItem>
-      <ListItemAvatar>
-        <Avatar>
-          <ImageIcon />
-        </Avatar>
-      </ListItemAvatar>
-      <ListItemText primary="Photos" 
-      secondary="Jan 9, 2014" />
+      <ListItemText primary=" No Notifications Yet" 
+      secondary=" " />
     </ListItem>
     <Divider variant="inset" 
     component="li" />
-    <ListItem>
-      <ListItemAvatar>
-        <Avatar>
-          <WorkIcon />
-        </Avatar>
-      </ListItemAvatar>
-      <ListItemText primary="Work" 
-      secondary="Jan 7, 2014" />
-    </ListItem>
-    <Divider variant="inset"
-     component="li" />
-    <ListItem>
-      <ListItemAvatar>
-        <Avatar>
-          <BeachAccessIcon />
-        </Avatar>
-      </ListItemAvatar>
-      <ListItemText primary="Vacation" 
-      secondary="July 20, 2014" />
-    </ListItem>
     <ListItem>
     <NextLink
       href="/notifications"
